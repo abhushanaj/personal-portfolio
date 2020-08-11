10b8f334c7f11ecc693dbc7819df83cc573d4abc
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2020-08-11 22:24:12 +0530

    Completion of the header section on homepage and splitting sociallinks into respective components

---
+++ src/components/social-links/social-link.constants.js
@@ -0,0 +1,6 @@
+export const githubURL = "https://github.com/abhu-A-J";
+
+export const linkedInURL =
+  "https://www.linkedin.com/in/abhushan-adhikari-joshi-157530176/";
+
+export const twitterURL = "https://twitter.com/AbhushanJ";
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,9 +1,38 @@
 import React from "react";
 
+import * as SocialLink from "../../components/social-links/social-links.component";
+
+import coolReactGuy from "../../assets/person.png";
+
 import "./homepage.styles.scss";
 
 const HomePage = () => {
-  return <h2>HomePage</h2>;
+  return (
+    <header className="header u-container">
+      <div className="header__content">
+        <h1 className="main-heading">
+          Hi!
+          <br />
+          I'm Abhushan.
+        </h1>
+        <p className="tagline">
+          Frontend Developer with a focus on building fast, robust and optimized
+          User Experiences. I believe in developing Interfaces which bring a
+          smile.
+        </p>
+      </div>
+
+      <div className="header__image">
+        <img src={coolReactGuy} alt="Illustration of guy wearing react swag" />
+      </div>
+
+      <div className="social-links">
+        <SocialLink.Github />
+        <SocialLink.LinkedIn />
+        <SocialLink.Twitter />
+      </div>
+    </header>
+  );
 };
 
 export default HomePage;

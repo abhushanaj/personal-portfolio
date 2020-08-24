1e06ad63fbaa4841293675a553760b9a52c1f7de
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2020-08-24 13:01:43 +0530

    Updating the about-me section on the about page for the portfolio

--- src/components/about-me-description/about-me-description.component.jsx
+++ src/components/about-me-description/about-me-description.component.jsx
@@ -7,24 +7,20 @@ const AboutMeDescription = () => {
     <>
       <h4 className="about-title">About Me</h4>
       <p className="about-desc">
-        Hi! I'm Abhushan, an Electronics and Communiction Undergrad who went
-        rogue to accomplish a long awaited passion of becoming a Web Software
-        Engineer.
+        Hi! I'm Abhushan, an Electronics and Communication Undergrad went rogue
+        turned Software Developer with a focus on building fast, robust, and
+        scalable User Experiences and Interfaces.
       </p>
       <p className="about-desc">
-        I love building fast, robust and optimized Web Interfaces and always
-        look forward to working on all things Web.
+        I'm based in Shillong, Meghalaya, India. I am a self-taught, passionate,
+        and hardworking Web Developer looking forward to collaborating and
+        learning from engineers much smarter than me while building products
+        that solve a problem and are used by thousands.
       </p>
       <p className="about-desc">
-        I'm an engineer based in Shillong, Meghalaya ,India. I am a self-taught,
-        passionate and hardworking Web Developer looking forward to
-        collaborating and learning from engineer's much smarter than me while
-        building products used by people.
-      </p>
-      <p className="about-desc">
-        I always love working within a team and I see myself working as a Senior
-        Developer, architecting highly performant web applications potentially
-        being used by thousands of people in the near future.
+        I enjoy working on all things Web, and always look forward to meeting
+        new people, learning, and talking about my interests and passion over a
+        cup of coffee.
       </p>
     </>
   );
